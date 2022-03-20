7d2fda7cc329f07f42c702d3bb26233b8e1b395b
yamadaitsuki <yamada351035@gmail.com>
2022-03-20 22:06:22 +0900

    build: yarn add react-router-dom

--- app/psychological-test/src/index.jsx
+++ app/psychological-test/src/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import RouterConfig from "./RouterConfig";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterConfig />
   </React.StrictMode>,
   document.getElementById("root")
 );
