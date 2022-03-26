12a01ebf1dc10ca5b510306d2196adf2add6d97a
yamadaitsuki <yamada351035@gmail.com>
2022-03-26 22:05:59 +0900

    feat: add Routing

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,25 +1,25 @@
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 import {
   Link as ChakraLink,
-  Text,
-  Code,
   List,
   ListIcon,
   ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+  Text,
+} from '@chakra-ui/react';
+import { Container } from '../components/Container';
+import { CTA } from '../components/CTA';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { Footer } from '../components/Footer';
+import { Hero } from '../components/Hero';
+import { Main } from '../components/Main';
 
 const Index = () => (
   <Container height="100vh">
-    <Hero />
+    <Hero title="心理テストApp" />
     <Main>
       <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
+        心理テストアプリです。
+        下記のリストから受けたい心理テストを選択してください。
       </Text>
 
       <List spacing={3} my={0}>
@@ -49,6 +49,6 @@ const Index = () => (
     </Footer>
     <CTA />
   </Container>
-)
+);
 
-export default Index
+export default Index;
---
+++ front/src/pages/result/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>test画面</div>;
+};
+
+export default index;
