84ed07f51118980e069297a5f497b5331c144e01
yamadaitsuki <yamada351035@gmail.com>
2022-03-27 18:18:33 +0900

    refactor: nav link

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -55,9 +55,13 @@ const NavBar = (props) => {
         justify="space-between"
       >
         <Flex align="center" mr={5}>
-          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-            Psychological-Test
-          </Heading>
+          <NextLink href="/" passHref>
+            <a>
+              <Heading size="lg" letterSpacing={'tighter'}>
+                Psychological-Test
+              </Heading>
+            </a>
+          </NextLink>
         </Flex>
 
         <Stack
